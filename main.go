df6f09366a95cea3675bc072574cbd149c80865b
jkskj <lllljkskj@163.com>
2023-07-13 23:29:53 +0800

    feat

--- main.go
+++ main.go
@@ -9,11 +9,8 @@ import (
 
 func onlyForV2() Gout.HandlerFunc {
 	return func(c *Gout.Context) {
-		// Start timer
 		t := time.Now()
-		// if a server error occurred
 		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
 		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
 	}
 }
@@ -24,7 +21,7 @@ func main() {
 		c.JSON(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 	r.GET("/hello/:name", func(c *Gout.Context) {
-		c.JSON(http.StatusOK, Gout.H{"filepath": c.Param("name")})
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
 	r.GET("/hello/*file", func(c *Gout.Context) {
 		c.JSON(http.StatusOK, Gout.H{"file": c.Param("file")})
@@ -33,7 +30,6 @@ func main() {
 	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *Gout.Context) {
-			// expect /hello/geektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
 	}
